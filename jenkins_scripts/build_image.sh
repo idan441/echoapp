65996cdf99a2e57e3d02e9c0561c19f3236d5e58
idan441 <idan441@gmail.com>
2020-02-11 17:32:42 +0200

    try

--- jenkins_scripts/build_image.sh
+++ jenkins_scripts/build_image.sh
@@ -34,7 +34,7 @@ echo "the image will be tagged as - ${imagetag}"
 
 #Now build the image. 
 #The docker command assumes you are located at the directory with the dockerfile. 
-docker build -t ${appname}:${imagetag} ../.
+sudo docker build -t ${appname}:${imagetag} ../.
 
 echo "finished building the image, end of shell script. " 
 
